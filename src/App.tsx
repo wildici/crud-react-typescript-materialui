d1d5bfbd2743c20b3807cae9e44fd775c18ada1c
Wildici Lins <wildici@hotmail.com>
2022-03-20 14:20:02 -0300

    [feat] add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import {BrowserRouter} from "react-router-dom";
+import { AppRouter } from "./routes";
+
 export const App = () => {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export {};
+import {Routes, Route, Navigate} from "react-router-dom";
+
+export const AppRouter = () => {
+    return(
+        <Routes>
+            <Route path="/pagina-inicial" element={<p>Página Inicial</p>} />
+
+            <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+        </Routes>
+    );
+}
