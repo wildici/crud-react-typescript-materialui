8c89d0d729b787747cee09b3677356e41763fb07
Wildici Lins <wildici@hotmail.com>
2022-03-20 14:56:54 -0300

    [feat] add materialui and light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import {ThemeProvider} from "@mui/material";
 import {BrowserRouter} from "react-router-dom";
+
+import {LightTheme} from "./shared/themes";
 import { AppRouter } from "./routes";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
